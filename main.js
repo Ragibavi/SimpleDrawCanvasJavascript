63140b9f03802f39cd59322428d3ac29843273b7
Ragibavi <syams280606@gmail.com>
2023-11-29 13:00:07 +0700

    addFile

--- main.js
+++ main.js
@@ -17,10 +17,38 @@ const ctx = canvas.getContext('2d')
 let isDrawing = false
 colorBtn = document.querySelector('.color-pick .option')
 
+
+
 const resizeCanvas = () => {
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-}
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const image = new Image();
+    image.onload = function () {
+        const aspectRatio = image.width / image.height;
+        const maxWidth = canvas.width * 0.8;
+        const maxHeight = canvas.height * 0.8;
+
+        let width = maxWidth;
+        let height = maxWidth / aspectRatio;
+
+        if (height > maxHeight) {
+            height = maxHeight;
+            width = maxHeight * aspectRatio;
+        }
+
+        let x = (canvas.width - width) /1.5;
+        let y = (canvas.height - height) / 2;
+
+        if(x < 266.61155866900174){
+         x = (canvas.width - width) / 2;
+         y = (canvas.height - height) / 2;
+        }
+        console.log(x)
+        ctx.drawImage(image, x, y, width, height);
+    };
+};
+
 window.addEventListener('resize', resizeCanvas)
 resizeCanvas()
 
